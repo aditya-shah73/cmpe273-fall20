1c4905391d26bad4fe5b2884131f6745cf056cfc
Aditya Shah <aditya7395@gmail.com>
2020-11-05 15:34:38 -0800

    Added base code for GET and POST endpoints

--- project/datastore_app/controllers/datastore_controller.py
+++ project/datastore_app/controllers/datastore_controller.py
@@ -4,3 +4,22 @@ from flask import render_template, request, jsonify, make_response, abort, Respo
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/api/entries', methods=['GET', 'POST'])
+def entries():
+    if request.method == 'GET':
+        print('GET')
+        return {
+            "key": "foo",
+            "value": "bar"
+        }
+    elif request.method== 'POST':
+        if not request.json:
+            abort(400)
+        print(request.json['key'])
+        print(request.json['value'])
+        return {'value': request.json['value']}, 201
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': '400 Bad Request'}), 400)
